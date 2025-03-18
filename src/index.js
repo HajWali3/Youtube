7344d951990144caf02490907b45b0ec9a5713aa
Haj Wali <hajwalii3@gmail.com>
2025-03-18 17:46:58 +0500

    add user and video model

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import dotenv from "dotenv";
 import express from "express";
 import connectDb from "./db/index.js";
-import mongoose from "mongoose";
-import { DB_NAME } from "./constants.js";
 const app = express();
 
 dotenv.config();
