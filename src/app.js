6dc429048219ce6b3051a053ef399edafc661d8e
Haj Wali <hajwalii3@gmail.com>
2025-03-29 22:50:57 +0500

    add registeer user

--- src/app.js
+++ src/app.js
@@ -16,4 +16,17 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+// routes import
+import userRouter from "./routes/user.route.js";
+
+// routes declaration
+// app.use("/api/v1/users", userRouter);
+app.use("/users", userRouter);
+
+app.get("/home", (req, res) => {
+  console.log("home");
+
+  res.send("home");
+});
+
 export { app };
--- src/index.js
+++ src/index.js
@@ -1,33 +1,15 @@
 import dotenv from "dotenv";
-import express from "express";
-import connectDb from "./db/index.js";
-const app = express();
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config();
 
-connectDb()
+connectDB()
   .then(() => {
-    app.listen(process.env.PORT || 8000, () => {
+    app.listen(process.env.PORT || 8080, () => {
       console.log(`Server is running at port ${process.env.PORT}`);
     });
   })
   .catch((err) => {
-    console.log("❌ MongoDB Connection FAILED", error);
+    console.log("MongoDB connection failure !!!", err);
   });
-
-// (async () => {
-//   try {
-//     mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-//     app.on("errror", (error) => {
-//       console.log("ERROR", error);
-//       throw error;
-//     });
-
-//     app.listen(process.env.PORT, () => {
-//       console.log(`App is listening on port ${process.env.PORT}`);
-//     });
-//   } catch (error) {
-//     console.error("ERRORl", error);
-//     throw error;
-//   }
-// })();
